2828504e5bcd4371cbc485446d3de5d24260e8e6
Angel D. Munoz <scythes.lol@gmail.com>
2021-08-23 00:13:41 -0500

    move app to it's own module register components on main

--- src/Main.fs
+++ src/Main.fs
@@ -1,36 +1,9 @@
 ﻿module Main
 
-open Browser.Types
-
-open Lit
-
-open Fable.Haunted
-
-open Types
-open Components
 open Pages
+
+// register your custom elements here
 
 Home.register ()
 Notes.register ()
-
-let App () =
-    let page, setPage = Haunted.useState (Page.Home)
-
-    let onBackRequested _ = printfn "Back requested"
-
-    let goToPage page _ = setPage page
-
-    let getPage page =
-        match page with
-        | Page.Home -> html $"""<flit-home></flit-home>"""
-        | Page.Notes -> html $"""<flit-notes></flit-notes>"""
-
-    html
-        $"""
-        <article>
-            {Navbar.View onBackRequested goToPage}
-            <main id="content">{getPage page}</main>
-        </article>
-        """
-
-defineComponent "flit-app" (Haunted.Component App)
+App.register ()
